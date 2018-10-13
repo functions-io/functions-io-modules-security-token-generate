c823de5aae1742b716fe0b0e7e8d8c87673fc1cb
Rogério Dias Moreira <rogeriodegoiania@gmail.com>
2018-10-13 00:30:08 -0300

    refactoring

--- examples/lab1.js
+++ examples/lab1.js
@@ -3,8 +3,18 @@
 const core = require("functions-io-core");
 const invokeFactory = core.buildInvokeFactory();
 const context = invokeFactory.buildContext();
-
 const moduleTest = require("../");
+
+context.getConfig = function(){
+    return {
+        secretOrPrivateKey : "aaaa",
+        algorithm : "HS256",
+        expiresIn : 3600,
+        issuer: "functions-io",
+        loginModuleName: "@functions-io-modules/security.login.mongo",
+        loginModuleVersion: "1.*"
+    };
+}
 
 var message1 = {};
 message1.username = "admin";
--- test/test.js
+++ test/test.js
@@ -7,6 +7,17 @@ const context = invokeFactory.buildContext();
 const assert = require("assert");
 const moduleTest = require("../");
 
+context.getConfig = function(){
+    return {
+        secretOrPrivateKey : "aaaa",
+        algorithm : "HS256",
+        expiresIn : 3600,
+        issuer: "functions-io",
+        loginModuleName: "@functions-io-modules/security.login.mongo",
+        loginModuleVersion: "1.*"
+    };
+}
+
 var message1 = {};
 message1.username = "admin";
 message1.password = "123";
